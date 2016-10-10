87b691313359b65a0d375f5a1eadffae9d925c26
gsalisi <geoffreysalisi@gmail.com>
2016-10-09 17:25:03 -0700

    Added testimonials, improved styles, added firebase

--- js/app.js
+++ js/app.js
@@ -1,10 +1,33 @@
+window.addEventListener("load", main);
 
-'use strict';
+function main() {
 
-$(function(){
-  //TODO:REMOVE
-  // $('.js-main-content').removeClass('hidden');
+  let db = {};
+  let dbLink = {}; 
 
+  firebase.initializeApp({
+    apiKey: "AIzaSyBaDKvobTip5-6JGCPLHTEkzHOaIkxqnQ4",
+    authDomain: "personal-website-b1da1.firebaseapp.com",
+    databaseURL: "https://personal-website-b1da1.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "61095216394"
+  });
+
+  // firebase.auth().signInWithEmailAndPassword('public@public.com', 'public');
+
+  const firebaseDB = firebase.database();
+  firebase.auth().signInAnonymously();
+
+  firebaseDB.ref('links').once('value').then((snapshot) => {
+    dbLink = snapshot.val();
+  });
+  firebaseDB.ref('works_and_skills').once('value').then((snapshot) => {
+    db = snapshot.val();
+  });
+
+  firebaseDB.ref('testimonials').once('value').then((snapshot) => {
+   startTestimonialShuffle(snapshot.val());
+  });
   let hasShownCaret = false;
   const $gsLogo = $('.js-logo');
   const $modal = $('.js-modal');
@@ -12,86 +35,95 @@ $(function(){
   const $link = $('.js-link');
 
   $('body').imagesLoaded(startLoadAnimation);
-
   $modalToggler.click(createModal);
   $link.click(openLink);
 
   function startLoadAnimation() {
-    window.setTimeout(() => {
-      $gsLogo.snabbt({
-        fromOpacity: 0,
-        opacity: 1,
-        fromScale: [0.9, 0.9],
-        duration: 500,
-        easing: 'ease'
-      });
-      window.setTimeout(() => {
-        if(!hasShownCaret) {
-          hasShownCaret = true;
-          showCaret();
-          $('.js-main-content').removeClass('hidden');
-        }
-        swivelLogo();
-      }, 500);
-    }, 100);
-  }
-
-  function swivelLogo() {
     $gsLogo.snabbt({
-      rotation: [0, Math.PI, 0],
-      easing: 'easeOut',
-      duration: 600
-    }).snabbt({
-      fromRotation: [0, Math.PI, 0],
-      rotation: [0, 2*Math.PI, 0],
-      easing: 'easeOut',
-      duration: 1000,
-      complete() {
-        rotateLogo();
-      }
-    }); 
-  }
-
-  function rotateLogo() {
-    $gsLogo.snabbt({
-      fromRotation: [0, 0, 0],
-      rotation: [0, 2 * Math.PI, 0],
-      duration: 15000,
-      complete() {
-        rotateLogo();
-      }
-    }); 
-  }
-
-  function showCaret() {
-    const $caret = $('.js-caret-container');
-    $caret.snabbt({
       fromOpacity: 0,
       opacity: 1,
       fromScale: [0.9, 0.9],
-      fromPosition: [0, -10, 0],
-      easing: 'ease',
-      complete() {
-        nudgeCaret();
-      }
+      duration: 500,
+      easing: 'ease'
     });
+    window.setTimeout(() => {
+      if(!hasShownCaret) {
+        hasShownCaret = true;
+        showCaret();
+        $('.js-main-content').removeClass('hidden');
+      }
+      swivelLogo();
+    }, 500);
+
+    function swivelLogo() {
+      $gsLogo.snabbt({
+        rotation: [0, Math.PI, 0],
+        easing: 'easeOut',
+        duration: 600
+      }).snabbt({
+        fromRotation: [0, Math.PI, 0],
+        rotation: [0, 2*Math.PI, 0],
+        easing: 'easeOut',
+        duration: 1000,
+        complete() {
+          rotateLogo();
+        }
+      }); 
+    }
+
+    function rotateLogo() {
+      $gsLogo.snabbt({
+        fromRotation: [0, 0, 0],
+        rotation: [0, 2 * Math.PI, 0],
+        duration: 15000,
+        complete() {
+          rotateLogo();
+        }
+      }); 
+    }
+
+    function showCaret() {
+      const $caret = $('.js-caret-container');
+      $caret.click(() => {
+        animatedScrollTo( 
+          $('.js-fixed-wrapper')[0], // element to scroll with (most of times you want to scroll with whole <body>)
+          $('.js-main-content')[0].getBoundingClientRect().top, // target scrollY (0 means top of the page)
+          1000, // duration in ms
+          function() { // callback function that runs after the animation (optional)
+            console.log('done!')
+          }
+        );
+      });
+      $caret.snabbt({
+        fromOpacity: 0,
+        opacity: 1,
+        fromScale: [0.9, 0.9],
+        fromPosition: [0, -10, 0],
+        easing: 'ease',
+        complete() {
+          nudgeCaret();
+        }
+      });
+    }
+
+    function nudgeCaret() {
+      const $caret = $('.js-caret-container');
+      $caret.snabbt({
+        position: [0, -10, 0],
+        delay: 500,
+        easing: 'ease'
+      }).snabbt({
+        fromPosition: [0, -10, 0],
+        position: [0, 0, 0],
+        easing: 'ease',
+        complete() {
+          nudgeCaret();
+        }
+      });
+    }
   }
 
-  function nudgeCaret() {
-    const $caret = $('.js-caret-container');
-    $caret.snabbt({
-      position: [0, -10, 0],
-      delay: 500,
-      easing: 'ease'
-    }).snabbt({
-      fromPosition: [0, -10, 0],
-      position: [0, 0, 0],
-      easing: 'ease',
-      complete() {
-        nudgeCaret();
-      }
-    });
-  }
+  
 
   function createModal(event) {
     $modal.addClass('md-show');
@@ -146,126 +178,38 @@ $(function(){
 
   function openLink(event) {
     const key = $(event.target).attr('data');
-    window.open(linkDb[key]).focus();
+    window.open(dbLink[key]).focus();
   }
 
-  const linkDb = {
-    github: 'https://github.com/gsalisi',
-    linkedin: 'https://www.linkedin.com/in/geoffreysalisi',
-    resume: 'http://geoffreysalisi.com/gsalisi_resume.pdf'
-  };
-  const db = {
-    frontend: {
-      label: 'Frontend Engineering',
-      description: 'Years of experience in building user interfaces for web, mobile and native applications',
-      bullets: [
-        '2+ years of hands-on experience in AngularJS 1.X-- proficiency in all its features and implementation',
-        'Follows up-to-date best practices and developments in the HTML, CSS and JavaScript community',
-        'Uses a wide array of front-end tools such as npm, grunt, gulp, browserify, babel, less etc.',
-        'Takes great pride in creating beautiful and polished user interfaces that allows for a pleasurable user experience',
-        'Used to develop Android applications and an IDE for SAP in Java',
-        ]
-    }, backend: {
-      label: 'Backend Engineering',
-      description: '3 internships worth of full-stack application development experience',
-      bullets: [
-        'Strong object-oriented programming understanding and other software design patterns',
-        'Developed web application features that required changes on the frontend, API layer, service layer and database',
-        'Used server-side technologies such as Flask, Spring MVC, Thrift and NodeJS',
-        'Maintained a multi-platform low-level application that runs natively in Linux, Mac and Windows',
-      ]
-    }, core: {
-      label: 'BS Computer Engineering',
-      description: 'University of Waterloo, Expected Graduation May 2017',
-      bullets: [
-        'Learned and implemented adaptive and cooperative algorithms such as PSO and ACO',
-        'Gained fundamental distributed computing concepts and developed basic Hadoop and Spark projects',
-        'Practical understanding of different search algorithms, data structures and their runtime/space complexities',
-        'Understands lower level computing concepts such as OS, compilers, memory allocation, processes and threads',
-        'Experenced with command line tools, linux bash environment and even circuits!'
-        ]
-    }, others: {
-      label: 'Other software related interests',
-      description: 'Some new/old things that I aspire to learn or use!',
-      bullets: [
-        'Learn React framework to keep up with the new front-end trends', 
-        'Try React Native; it looks promising with its goal to unify the frontend with none other than JavaScript',
-        'Desire to try out mobile application development again, possibly IOS.',
-        'Gain more experience with database technologies such as PostgreSQL and SQL'
-      ]
-    },  dance: {
-      label: 'Urban Hiphop Choreography',
-      description: '6 years of dancing to urban hiphop choreography competitively and as a hobby',
-      bullets: [
-        'Competed for Bring your own Beat (BYOB 2014), Ontario University Competition for HipHop (OUCH 2012, 2013 and 2015)',
-        'Co-directed OUCH competitive teams: Origins and Kindred Culture',
-        'Teaches and dances with University of Waterloo Hiphop Club since 2012',
-        'Joined University of the Philippines Los Banos Street Jazz Dance Company in 2010 to 2011',
-        'Choreographed a set for the charity fashion show of Fashion for Change in 2014 and 2015'
-      ],
-      link: {
-        label: 'Oh really?',
-        href: 'https://www.youtube.com/playlist?list=PLPoHClbCHMj2Yr9EEYynUsa9Z5AKb37M0'
+  function startTestimonialShuffle(dbTestimonial) {
+    const testi = $('.js-testi');
+    const testiCtr = $('.js-testi-text');
+    const testiOwnerCtr = $('.js-testi-owner');
+
+    let i = 0;
+    testiCtr.text(dbTestimonial[i].text);
+    testiOwnerCtr.text(dbTestimonial[i].owner);
+    i++;
+    function shuffleTesti() {
+      if (i >= dbTestimonial.length) {
+        i = 0;
       }
-    }, work_delphix: {
-      label: 'Frontend Engineering Intern',
-      description: 'Delphix :: San Francisco, California :: September 2016 - December 2016',
-      bullets: [
-        'Develops and maintains the user interface of the main Delphix Engine',
-        'Uses AngularJS and Knockout.js as the main JavaScript frameworks/tool',
-      ],
-      link: {
-        label: 'More info',
-        href: 'https://www.delphix.com'
-      }
-    }, work_compass: {
-      label: 'Software Engineering Intern',
-      description: 'Compass :: New York, New York :: January 2016 - July 2016',
-      bullets: [
-        'Created an automated tour itinerary builder with optimized routes: Leveraged several Google APIs; cuts time to plan tour from hours to minutes',
-        'Created an Angular static map service that wraps Google\'s directions service that creates a map with markers and polylines for directions',
-        'Implemented the full-stack search functionality for our Documents center',
-        'Modified Java Thrift server; Added MongoDB indexing; Created front-end search bar component',
-        'Helped build a pdf export tool that renders HTML, converts it to PDF and downloads it'
-      ],
-      link: {
-        label: 'More info',
-        href: 'https://www.compass.com'
-      }
-    }, work_sap: {
-      label: 'Software Developer, In Memory',
-      description: 'SAP Canada :: Waterloo, ON :: May 2015 - August 2015',
-      bullets: [
-        'Worked on SAP Smart Data Streaming (SDS) Development Studio based on Eclipse Platform',
-        'Integrated JUnit tests to the E2E test harness using bash script and Python',
-        'Developed and tested software for Windows and Linux OS locally and on a VM remotely',
-        'Maintained different parts of the stack such as the streaming server, SDK, compiler, etc.'
-      ],
-      link: {
-        label: 'More info',
-        href: 'http://help.sap.com/hana_options_sds/'
-      }
-    }, work_rbc: {
-      label: 'Fullstack Software Developer',
-      description: 'Royal Bank of Canada, Capital Markets :: Toronto, Ontario :: May 2015 - August 2015',
-      bullets: [
-        'Built Market Risk web applications using Java Spring MVC, HTML, CSS and Javascript',
-        'Created an Angular Boilerplate and security testing for as proof of concept',
-        'Closely worked with a designer in building the user interface and enhance user experience'
-      ], 
-      link: {
-        label: 'More info',
-        href: 'http://campus.rbccm.com/en/home/our-programs/risk-management/'
-      }
-    }, work_other: {
-      label: 'Other work experience',
-      bullets: [
-        'Technical Support Representative - University of Waterloo',
-        'Desktop Support Technician - Ontario Hospital\'s Association',
-        'General Labourer - Wing\'s Food Product',
-        'Park Sweeper - Canada\'s Wonderland'
-      ]
+      testi.snabbt({
+        fromOpacity: 0,
+        opacity: 1,
+      }).snabbt({
+        delay: dbTestimonial[i].length,
+        fromOpacity: 1,
+        opacity: 0,
+        complete() {
+          testiCtr.text(dbTestimonial[i].text);
+          testiOwnerCtr.text(dbTestimonial[i].owner);
+          i++;
+          shuffleTesti();
+        }
+      }); 
     }
-  };
-  
-});
+    shuffleTesti();
+  }
+}
+  //end of main
